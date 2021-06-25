51b64ce9748b66016b0edb0ac904600d07a58545
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2021-06-25 23:57:49 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,7 @@
 class Backer
   attr_accessor :name, :backed_projects
   
-  def initialize(name)
+  def initialize(name) #takes a name on initialization, accessible through an attribute reader
     @name = name
     @backed_projects=[]
   end
