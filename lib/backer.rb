5e0ade9ea8e8d3188b7dc1c51f26922c3ffa50b6
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2021-06-25 23:58:28 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,6 +8,6 @@ class Backer
   
   def back_project(project) #accepts a Project as an argument and stores it in a backed_projects array
     @backed_projects << project
-    project.add_backer(self)
+    project.add_backer(self) #also adds the backer to the project's backers array
   end
 end
